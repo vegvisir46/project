c70ed674e985e91562e0b4a36ec1e4405de6d8e6
vegvisir <alexandr.rostow@gmail.com>
2021-10-31 17:48:06 +0700

    les12

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 "use strict";
 
 
-let numberOfFilms = prompt("How much films do u see?");
+const numberOfFilms = +prompt('How much films do u see?', '');
 console.log (numberOfFilms);
 
-let personalMovieDB = {
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
-}
+};  
 
-personalMovieDB.movies = prompt();
+let q1 = prompt('Фильм:', ''),
+    q2 = prompt('Оценка:', ''),
+    q3 = prompt('Фильм:', ''),
+    q4 = prompt('Оценка:', '');
+
+personalMovieDB.movies [q1] = q2;
+personalMovieDB.movies [q3]= q4;
+
+//console.log(personalMovieDB.movies.q1);
+//console.log(personalMovieDB.movies.q3);
+console.log(personalMovieDB);
