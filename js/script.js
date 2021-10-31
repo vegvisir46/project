49e549de126b59a2d8152c5df2ceecafe220f91d
vegvisir <alexandr.rostow@gmail.com>
2021-10-31 12:22:26 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//let a = confirm("Cho kak?");
+//console.log(a);
+
+let bbb = prompt("DR", "xx.xx.xxxx");
+console.log(bbb);
+
+
+
+//let number = 5;
+//const leftBorderWidth = 1;
+//const leftBorderWidth = 5;
+
+
+
+//console.log(number);
