1107cf76086894f6a903c65c503a853f9202b499
vegvisir <alexandr.rostow@gmail.com>
2021-11-06 11:20:22 +0700

    3rd commit

--- js/script.js
+++ js/script.js
@@ -1,25 +1,273 @@
-"use strict";
+"use strict"
 
+//***********************************
+//_________П Р А К Т И К А____________
+/*
+let numberOfFilms;
 
-const numberOfFilms = +prompt('How much films do u see?', '');
-console.log (numberOfFilms);
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+start();
+
+let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
-};  
+}
 
-let q1 = prompt('Фильм:', ''),
-    q2 = prompt('Оценка:', ''),
-    q3 = prompt('Фильм:', ''),
-    q4 = prompt('Оценка:', '');
+function detectPersonalLevel(){
+    let stat = (personalMovieDB.count < 10) ? "Просмотрено довольно мало фильмов" :
+    (personalMovieDB.count <= 30) ? "Вы классический зверь" :
+    (personalMovieDB.count > 30) ? "Вы киноман" : "Произошла ошибка";
+    alert (stat);
+}
+detectPersonalLevel();
 
-personalMovieDB.movies [q1] = q2;
-personalMovieDB.movies [q3]= q4;
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let q1 = prompt ('Один из последних просмотренных фильмов?', '');
+        let q2 = prompt ('На сколько оцените его?', '');
+        
+        if((q1 == null) || (q2 == null) || (q1.length>5) || (q2.length>5) || (q1 == "") || (q2 == "")){
+            i--;
+            alert("Возможные варианты ошибки:\n1. Длина\n2. Пустой ответ\n3. Вы нажали отмена");
+        } else {personalMovieDB.movies[q1] = q2;}
+    }
+}
+rememberMyFilms();
 
-//console.log(personalMovieDB.movies.q1);
-//console.log(personalMovieDB.movies.q3);
-console.log(personalMovieDB);
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++){
+        personalMovieDB.genres [i-1] = prompt (`Ваш любимый жанр под номером ${i}`,'');
+    }
+}
+writeYourGenres();
+
+function showMyDB(){
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB();
+
+*/
+
+
+
+
+//***********************************
+//_________O B J E C T S____________
+/*
+//const obj = new Object();
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
+};
+
+//console.log(options.colors.vvv.bbb);
+//console.log(options["colors"]["vvv"]["bbb"]);
+//delete options.colors.bg;
+
+console.log(Object.keys(options).length);
+
+options.makeTest();
+
+const {border, bg} = options.colors;
+console.log(border);
+
+
+let counter = 0;
+for (let key in options) {
+    counter++;
+    if (typeof(options[key]) === 'object')  {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`)
+        }
+    }   else {
+           console.log(`Свойство ${key} имеет значение ${options[key]}`)
+          
+    }
+}
+console.log(counter);
+*/
+//***********************************
+
+
+//***********************************
+//_____________M E T H O D S____O F____S T R I N G S____A N D____N U M_____________
+/*
+let str = "testMEGATEST";
+
+
+console.log(str.toUpperCase());
+console.log(str);
+console.log(str.slice(5,10));
+console.log(str.substring(5,10));
+console.log(str.substr(5,3));
+//console.log(str[1]+str[3]);
+
+let num = "12.8px123";
+console.log(Math.round(num));
+console.log(parseInt(num));
+console.log(parseFloat(num));
+*/
+
+
+//____INSIGHT___CLOSURE_____
+/*
+function getCounter() { 
+    let counter = 0;
+    //console.log (counter);
+    return function() {
+        return counter++;
+    }
+}
+
+let count = getCounter();
+console.log(count());
+console.log(count());
+console.log(count());
+console.log(count());
+*/
+//***********************************
+
+
+
+
+//***********************************
+//_____________M A S S I V E S_____________
+/*
+const arr = [2, 13, 26, 8, 10];
+
+//arr[99] = 0; //пуш элемента на 99ую позицию массива
+//console.log(arr.length); //демонстрация сущности .length
+
+//arr.pop(); //удаление последнего значения массива
+//arr.push(10); //пуш нового элемента в конец массива
+
+
+// **виды переборов элементов массива**
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+} //перебор элементов массива
+
+for (let value of arr) {
+    console.log(value);
+    //тут можно юзать break и continue
+}
+
+arr.forEach(function(item, i, arr) {
+    console.log(`${i}: ${item} внутри массива ${arr}`);
+});
+
+
+// **формирование массива из строки и наоборот, сортировка**
+
+const str = prompt("","");
+const products = str.split(", ") //массив из строки
+console.log(products);
+console.log(products.join('; ')); //строка из массива
+
+
+products.sort(); //сортировка строчная
+
+arr.sort(compareNum); //сортировка численная
+function compareNum(a, b){
+    return a-b;
+}
+
+console.log(arr);
+*/
+
+
+//***********************************
+//_____________П Е Р Е Д А Ч А___П О___С С Ы Л К Е___И___З Н А Ч Е Н И Ю_____________
+/*
+const obj = {
+    a: 5,
+    b: 1
+};
+
+const copyy = obj; //ССЫЛКА !! на объект obj
+copy.a = 10;
+console.log(copyy);
+console.log(obj);
+*/
+
+//поверхностное клонирование
+/*
+function copy (mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+    return objCopy;
+}
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+}
+
+const newNumbers = copy(numbers);
+newNumbers.a = 10;
+console.log(numbers);
+console.log(newNumbers);
+
+//(numbers == newNumbers) ? alert("true") : alert("false");
+*/
+
+
+//тоже поверхностное клонирование
+/*
+const add = {
+    d: 17,
+    e: 20
+};
+const clone = Object.assign({}, add);
+clone.d = 20;
+
+console.log(add);
+console.log(clone);
+*/
+
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+newArray[1] = 'd';
+console.log(oldArray);
+console.log(newArray);
+
+
+//22 урок 17:261
+
+
+
+
+
+
+
+
